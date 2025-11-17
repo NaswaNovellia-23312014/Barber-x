2f7529ec237859b316fafdf03c152661c3872696
NaswaNovellia-23312014 <naswa_novellia@teknokrat.ac.id>
2025-11-18 06:21:45 +0700

    feat(api-admin): Buat endpoint PUT /api/admin/bookings/[id] (dilindungi)

---
+++ api/src/app/api/admin/bookings/[slug]/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server';
+import prisma from '@/lib/prisma';
+import { getAdminSession } from '@/lib/session'; // Panggil "Satpam"
+
+// Fungsi: PUT (Admin meng-update status booking)
+export async function PUT(
+    request: Request,
+    { params }: { params: { id: string } }
+) {
+  // Cek Login
+    const session = await getAdminSession();
+    if (!session) {
+    return NextResponse.json({ message: 'Tidak Terautentikasi' }, { status: 401 });
+    }
+
+  // Jika aman, jalankan logika
+    try {
+    // Admin hanya akan mengirim status baru, misal: { "status": "CONFIRMED" }
+    const body = await request.json(); 
+    const { status } = body; 
+
+    const updatedBooking = await prisma.booking.update({
+        where: { id: params.id },
+        data: {
+        status: status,
+        },
+    });
+    return NextResponse.json({ booking: updatedBooking });
+    } catch (error) {
+    return NextResponse.json({ message: 'Gagal update booking' }, { status: 500 });
+    }
+}
