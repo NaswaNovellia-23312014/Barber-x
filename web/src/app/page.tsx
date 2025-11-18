21140505c6ef7a1914736467e591ecc91ffe0fd5
Nazila Erza Hakim <nazila_erza_hakim@teknokrat.ac.id>
2025-11-18 23:00:10 +0700

    add text "selamat datang"

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -12,7 +12,14 @@ export default function Home() {
         {/* Overlay gelap */}
         <div className="absolute inset-0 bg-black/50"></div>
 
-        
+        {/* Teks */}
+        <div className="relative z-10 h-full flex flex-col justify-center items-center text-center px-4">
+          <h1 className="text-4xl md:text-6xl font-bold text-[#E9DFC7] drop-shadow-lg">
+            Selamat Datang di Barber-X
+          </h1>
+
+
+        </div>
       </section>
     </div>
   );
