b9e2fbb27a6ac1cda482d0436ca03c4aeafe6ee2
Nazila Erza Hakim <nazila_erza_hakim@teknokrat.ac.id>
2025-11-18 23:20:11 +0700

    add text wa

--- web/src/components/footer/Footer.tsx
+++ web/src/components/footer/Footer.tsx
@@ -29,6 +29,7 @@ export default function Footer() {
         {/* Contact */}
         <div>
           <h3 className="font-semibold text-lg mb-3 text-[#C8A36A]">Kontak</h3>
+          <p className="text-sm">Whatsapp: 0812-3456-7890</p>
 
         </div>
       </div>
