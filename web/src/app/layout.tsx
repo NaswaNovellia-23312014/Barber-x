6c2c0ab6ba10a7d34646c90186f7445b72067c3d
Nazila Erza Hakim <nazila_erza_hakim@teknokrat.ac.id>
2025-11-18 23:58:03 +0700

    Import Globals, Navbar & Footer

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,19 +1,6 @@
-import type { Metadata } from "next";
 import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
+import type { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Barber-X",
-  description: "Barbershop premium website",
-};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
