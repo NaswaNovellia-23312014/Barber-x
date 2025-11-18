4443c77c193838693f189334c0fd5bbe232ef420
Nazila Erza Hakim <nazila_erza_hakim@teknokrat.ac.id>
2025-11-18 23:59:04 +0700

    add title and informasi halaman utama

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -3,4 +3,9 @@ import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
 import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Barber-X",
+  description: "Website Barbershop Premium",
+};
+
 
