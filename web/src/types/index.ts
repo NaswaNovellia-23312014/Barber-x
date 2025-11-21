9068a14ea221b5cddd4de0f0d8ec585b109fe804
NaswaNovellia-23312014 <naswa_novellia@teknokrat.ac.id>
2025-11-22 03:28:54 +0700

    feat(types): Definisikan interface Service, Booking, dan Status

---
+++ web/src/types/index.ts
@@ -0,0 +1,22 @@
+export interface Service {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    duration: number; // Durasi layanan dalam menit
+}
+
+export interface Booking {
+    id: string;
+    customerName: string;
+    customerPhone: string;
+    bookingTime: string; // ISO string (misal "2023-12-25T14:00:00.000Z")
+    status: 'PENDING' | 'CONFIRMED' | 'CANCELLED'; // Status booking
+    serviceId: string;
+    // createdAt?: string; 
+}
+
+export interface User {
+    id: string;
+    username: string;
+}
