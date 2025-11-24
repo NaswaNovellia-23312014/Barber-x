00e67c2f24c900c0d7c1c8ae798e08833e4baa73
alexameyaksyah <alexameyaksyah@gmail.com>
2025-11-24 10:08:51 +0700

    feat(types): Tambahkan interface dasar untuk User/Token

--- web/src/types/index.ts
+++ web/src/types/index.ts
@@ -20,3 +20,15 @@ export interface User {
     id: string;
     username: string;
 }
+
+// Tipe data untuk respons sukses dari API login
+export interface AuthResponse {
+    token: string;
+    admin: User;
+    message: string;
+}
+
+// Tipe dasar untuk respons yang hanya berisi pesan
+export interface MessageResponse {
+    message: string;
+}
