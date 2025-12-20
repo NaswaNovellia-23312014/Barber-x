7cfc907ddae12cfa8f189096c38e5d2cee93dcbc
Nazila Erza Hakim <nazila_erza_hakim@teknokrat.ac.id>
2025-12-20 18:49:56 +0700

    ui: tambahkan link About pada menu mobile

--- web/src/components/navbar/Navbar.tsx
+++ web/src/components/navbar/Navbar.tsx
@@ -71,6 +71,8 @@ export default function Navbar() {
       {open && (
         <div className="md:hidden bg-[#F5ECD6] flex flex-col gap-4 px-6 py-4 border-t border-black/10 font-medium text-[#3F3F3F]">
                     <Link href="#home" onClick={() => setOpen(false)}>Home</Link>
+                              <Link href="#about" onClick={() => setOpen(false)}>About</Link>
+
 
         </div>
       )}
