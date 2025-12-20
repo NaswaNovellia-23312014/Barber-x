406429b04665331e28169f0f5eee0d81f195ff85
NaswaNovellia-23312014 <naswa_novellia@teknokrat.ac.id>
2025-12-20 21:42:50 +0700

    feat(config): implementasi header CORS dan aktivasi React Compiler

--- api/next.config.ts
+++ api/next.config.ts
@@ -1,8 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  async headers() {
+    return [
+      {
+        // Mencakup semua route API yang dimulai dengan /api
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          // Izinkan port 3000 (Frontend Anda)
+          { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" }, 
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
