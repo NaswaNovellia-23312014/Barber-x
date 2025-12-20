a4aef92580486dd8068f627b7b3e82819c62b6ec
Nazila Erza Hakim <nazila_erza_hakim@teknokrat.ac.id>
2025-12-20 18:50:53 +0700

    ui: tambahkan link contact pada menu mobile

--- web/src/components/navbar/Navbar.tsx
+++ web/src/components/navbar/Navbar.tsx
@@ -72,7 +72,10 @@ export default function Navbar() {
         <div className="md:hidden bg-[#F5ECD6] flex flex-col gap-4 px-6 py-4 border-t border-black/10 font-medium text-[#3F3F3F]">
                     <Link href="#home" onClick={() => setOpen(false)}>Home</Link>
                               <Link href="#about" onClick={() => setOpen(false)}>About</Link>
-          <Link href="#services" onClick={() => setOpen(false)}>Catalog</Link>
+                                        <Link href="#services" onClick={() => setOpen(false)}>Catalog</Link>
+
+          <Link href="#contact" onClick={() => setOpen(false)}>Contact</Link>
+
 
 
         </div>
